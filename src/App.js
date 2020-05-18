d8cb435ef275779aee05c895620031166ecb5225
sarmadkhan1991 <43259363+sarmadkhan1991@users.noreply.github.com>
2020-05-18 00:15:45 -0700

    added mdl

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Sarmad Khan
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
