4b04a85dcc4412313d8b7cf5fa31420fe505b851
sarmadkhan1991 <43259363+sarmadkhan1991@users.noreply.github.com>
2020-05-19 02:11:21 -0700

    landing page styled

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+import Sirmud from './Portfolio-pic.png';
 
 class LandingPage extends Component {
     render () {
         return (
-            <div>
-                <h1>Landing Page</h1>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img 
+                            src={Sirmud}
+                            alt='headShot'
+                            className='headShot-img'
+                        />
+                        <div className='banner-text'>
+                            <h1>Full Stack Web Developer</h1>
+
+                            <hr />
+
+                            <p>HTML5/CSS | NodeJS | JavaScript | ReactJS | Express | </p>
+
+                            <div className="social-links">
+
+                                {/* LinkedIn */}
+                                <a href="http://google.com" rel="noopener noreferrer"  target="_blank">
+                                    <i className="fa fa-linkedin-square" aria-hidden="true" />
+                                </a>
+
+                                {/* Github */}
+                                <a href="http://google.com" rel="noopener noreferrer"  target="_blank">
+                                    <i className="fa fa-github-square" aria-hidden="true" />
+                                </a>
+
+                                {/* YouTube */}
+                                <a href="http://google.com" rel="noopener noreferrer"  target="_blank">
+                                    <i className="fa fa-youtube-square" aria-hidden="true" />
+                                </a>
+
+                            </div>
+                        </div>
+                    </Cell>
+                </Grid>
             </div>
         )
     }
