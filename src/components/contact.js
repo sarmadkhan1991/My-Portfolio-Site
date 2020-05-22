62070ca979d6721a4beaae7bdf025a4288f75247
sarmadkhan1991 <43259363+sarmadkhan1991@users.noreply.github.com>
2020-05-22 02:11:08 -0700

    Contact page done

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
+import Sirmud from './Portfolio-pic.png';
 
 class Contact extends Component {
     render () {
         return (
-            <div>
-                <h1>Contact Page</h1>
+            <div className='contact-body'>
+                <Grid className='contact-grid'>
+                    <Cell col={6}>
+                        <h2>Sarmad Khan</h2>
+                        <img 
+                            src={Sirmud} 
+                            alt='avatar'
+                            style={{ height: '250px', borderRadius: '10px' }}
+                        />
+                        <p style={{width: '75%', margin: 'auto', paddingTop: '1em'}}>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                        </p>
+                    </Cell>
+                    <Cell col={6}>
+                        <h2>Contact Me</h2>
+                        <hr/>
+                        <div className='contact-list'>
+                            <List>
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                                        <i className='fa fa-phone-square' aria-hidden='true'/>
+                                        (623) 203-3317
+                                    </ListItemContent>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                                        <i className='fa fa-envelope' aria-hidden='true'/>
+                                        sir.mud.khan@gmail.com
+                                    </ListItemContent>
+                                </ListItem>
+                            </List>
+                        </div>
+                    </Cell>
+                </Grid>
             </div>
         )
     }
