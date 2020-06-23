96a0688e227756d151f0fe7462120d2b3ef85ba3
sarmadkhan1991 <43259363+sarmadkhan1991@users.noreply.github.com>
2020-06-22 17:32:09 -0700

    styling issue test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
 import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
